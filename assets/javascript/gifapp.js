b1da72db2297a4dd446cd98cd6bef5a73073d7de
Claude Meurehg <claude.meurehg@mail.utoronto.ca>
2018-05-15 12:15:35 -0400

    code to get gifs presents error.

--- assets/javascript/gifapp.js
+++ assets/javascript/gifapp.js
@@ -9,8 +9,8 @@ $(document).ready(function() {
     for (i=0; i < topics.length; i++) {
 
         var buttons = $("<button>");
-        //buttons.addClass("name-to-click");
-        //buttons.attr("data-name", topics[i]);
+        buttons.addClass("name-to-click");
+        buttons.attr("data-name", topics[i]);
         buttons.text(topics[i]);
         $("#button-place").append(buttons);
         }
@@ -34,7 +34,46 @@ $(document).ready(function() {
 
 //var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5");
 //xhr.done(function(data) { console.log("success got data", data); });
+    $(".name-to-click").on("click", function () {
+        var artist = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/upload.giphy.com/v1/gifs/search?q=" + artist + 
+        "&api_key=XHTj6YMhkDfZvYfpzr7eTKiKw3GE9d09&limit=10"
 
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+
+        })
+
+
+        .then(function (response) {
+            
+            var gotInfo = response.data;
+
+            for (i=0; i < gotInfo.length; i++) {
+                
+                var divImage = $("<div class='image-place'>");
+
+                var rating = gotInfo[i].rating;
+
+                var textLine = $("<p>").text("Rating:" + rating);
+
+                var gifImage = $("<img>");
+                gifImage.attr("src", gotInfo[i].images.fixed_height.url);
+
+            divImage.append(gifImage);
+            divImage.append(rating);
+
+            $("#gif-place").append(divImage);
+            
+
+            }
+
+        })
+
+
+
+    })
 
 
 });
